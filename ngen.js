5e5674e58e4de2ea5cac664bdaaaae31ffd95af3
프날 (Pnal) <contact@pnal.dev>
2024-01-02 23:44:35 +0900

    Initial commit

---
+++ ngen.js
@@ -0,0 +1,46 @@
+let toolbox = createElementWithId("form", "ngen-toolbox");
+let writebox = createElementWithId("iframe", "ngen-writebox");
+writebox.setAttribute("name", "ngenFrame");
+writebox.setAttribute("src", "about:blank");
+
+
+toolbox.append(createButtonWithCmd("bold"));
+
+document.getElementById("ngen-editor").append(toolbox);
+document.getElementById("ngen-editor").append(writebox);
+ngenFrame.document.designMode = "On";
+
+ngenFrame.document.open();
+ngenFrame.document.write("<br>");
+console.log(ngenFrame.document);
+ngenFrame.document.close();
+
+function createElementWithId(_type, _id) {
+    newDiv = document.createElement(_type);
+    newDiv.setAttribute("id", _id);
+    return newDiv;
+}
+
+function createButtonWithCmd(_cmd) {
+    newBtn = document.createElement("Button");
+    newBtn.setAttribute("type", "button");
+    newBtn.setAttribute("tool-cmd", _cmd);
+    return newBtn;
+}
+
+document.getElementById("ngen-toolbox").addEventListener("click", async(e) =>  {
+    const cmd = e.target.closest("button").getAttribute("tool-cmd");
+    ngenFrame.document.execCommand(cmd, false, null);
+    console.log(getHTML());
+});
+
+function getHTML() {
+    let result = "";
+    result = ngenFrame.document.body.innerHTML.replace(/<div>/g, "<p>").replace(/<\/div>/g, "</p>")
+    result = result.replace(/<p>\s*(<br>)?\s*<\/p>/g, "<br>")
+    result = result.replace(/<\/p><p>/g, "<br>")
+    result = result.replace(/<br><\/p>/g, "</p>")
+    result = result.replace(/<\/p>(<br>)+<p>/g, "</p><p>")
+
+    return result;
+}
